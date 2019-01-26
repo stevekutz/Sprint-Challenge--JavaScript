d08164b8fb9c17d32584996ba9f5a106149f7297
stevekutz <stkutz@gmail.com>
2019-01-26 12:11:36 -0600

    completed classes, will go back after STRETCH to make handle invalid inputs, e.g. -width, 0 width. etc.

--- challenges/classes.js
+++ challenges/classes.js
@@ -4,4 +4,71 @@
 // console.log(cuboid.volume()); // 100
 // console.log(cuboid.surfaceArea()); // 130
 
-// Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
+// Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.
+// Find out the formulas for volume and surface area for cubes and create those methods
+// using the dimension properties from CuboidMaker.
+// Test your work by logging out your volume and surface area.
+
+
+/* == Step 1: Base Constructor ==
+  Create a constructor function named CuboidMaker that accepts properties for length, width, and height
+
+function CuboidMaker (dim) {   // using dim obj for dimension attributes
+  this.length = dim.length;
+  this.height = dim.height;
+  this.width = dim.width;
+}*/
+
+class CuboidMaker {
+  constructor(dim) {
+    this.dimensions = dim.dimensions;  // more flexible way to describe dimension, in case of future refactor
+  }
+
+  volume() {
+    let vol = 0;
+    // test if dimensions is not an empty  { } or invalid
+    if(Object.keys(this.dimensions).length !== 0) {
+      vol = 1;                  // iterate over dimensions to provide volume
+      for(var key in this.dimensions)   vol *= this.dimensions[key];
+    }
+
+    return vol;
+  }
+
+  surfaceArea() {
+    let dim = this.dimensions;
+    let double = (val) => 2 * val;
+
+    if(Object.keys(this.dimensions).length !== 0) {
+      return double(dim.length*dim.width + dim.length*dim.width + dim.width*dim.height);
+    }
+
+    return 0; // invalid dim, surface area 0
+  }
+
+}
+let cuboid = new CuboidMaker({
+  dimensions: {
+    length: 4,
+    width: 5,
+    height: 5
+  }
+});
+
+let cuboidEmpty = new CuboidMaker({
+  dimensions: { }
+});
+
+let cuboidEmpty2 = new CuboidMaker({
+  dimensions: {
+    length: 10,
+  }
+});
+
+// Test your volume and surfaceArea methods by uncommenting the logs below:
+console.log(cuboid.volume()); // 100
+console.log(cuboid.surfaceArea()); // 130
+
+console.log(cuboidEmpty.volume()); //
+console.log(cuboidEmpty.surfaceArea()); // 130
+
